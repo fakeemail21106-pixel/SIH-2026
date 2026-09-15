21defae90e15672945013c868176c292ea64db7c
fakeemail21106-pixel <fakeemail21106@gmail.com>
2026-09-15 15:27:58 +0530

    Delete modules/aiAssistant/aiAssistant.routes.js

--- modules/aiAssistant/aiAssistant.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./aiAssistant.controller');
-
-router.post('/ask', controller.handleAiAsk);
-
-module.exports = router;
